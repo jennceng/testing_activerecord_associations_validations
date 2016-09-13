e1424da89570920f5f35e3e45e2551a3907b274d
Jenn Eng <Jenn@Jennifers-MacBook-Pro-2.local>
2016-09-13 19:13:32 -0400

    model test failing as it should

---
+++ spec/models/task_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe Task do
+  it { should have_valid(:title).when("a title") }
+  it { should_not have_valid(:title).when(nil, "") }
+
+  it { should have_valid(:user).when(User.new) }
+  it { should_not have_valid(:user).when(nil) }
+end
